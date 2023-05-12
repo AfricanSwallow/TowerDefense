1c0b6f66c09bd25cc279f711b955dd38ea89ad3c
AfricanSwallow <shrubbery@gapp.nthu.edu.tw>
2023-05-12 21:55:23 +0800

    TODO 4

--- ExplosionEffect.hpp
+++ ExplosionEffect.hpp
@@ -5,6 +5,7 @@
 #include <vector>
 
 #include "Sprite.hpp"
+#include "PlugGunTurret.hpp"
 
 class PlayScene;
 
@@ -16,6 +17,17 @@ protected:
 	float timeSpan = 0.5;
 public:
 	ExplosionEffect(float x, float y);
+	void Update(float deltaTime) override;
+};
+
+class ShootEffect : public Engine::Sprite {
+protected:
+	PlayScene* getPlayScene();
+	float timeTicks;
+	std::vector<std::shared_ptr<ALLEGRO_BITMAP>> bmps;
+	float timeSpan = 0.5;
+public:
+	ShootEffect(Engine::Point position, Engine::Point forwardDirection, float rotation, Turret* parent);
 	void Update(float deltaTime) override;
 };
 #endif // EXPLOSIONEFFECT_HPP

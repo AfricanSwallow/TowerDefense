f292987c443e22326262b398914bd8fa5594fad9
AfricanSwallow <shrubbery@gapp.nthu.edu.tw>
2023-05-12 23:21:45 +0800

    put GreenNormalEnemy in RedNormalEnemy files

--- RedNormalEnemy.cpp
+++ RedNormalEnemy.cpp
@@ -5,3 +5,7 @@
 RedNormalEnemy::RedNormalEnemy(int x, int y) : Enemy("play/enemy-10.png", x, y, 25, 45, 7, 5) {
     // TODO 2 (1/3): You can imitate the 2 files: 'RedNormalEnemy.hpp', 'RedNormalEnemy.cpp' to create a new enemy.
 }
+
+GreenNormalEnemy::GreenNormalEnemy(int x, int y) : Enemy("play/enemy-8.png", x, y, 25, 45, 7, 5) {
+    // TODO 2 (1/3): You can imitate the 2 files: 'RedNormalEnemy.hpp', 'RedNormalEnemy.cpp' to create a new enemy.
+}
--- RedNormalEnemy.hpp
+++ RedNormalEnemy.hpp
@@ -6,4 +6,9 @@ class RedNormalEnemy : public Enemy {
 public:
     RedNormalEnemy(int x, int y);
 };
+
+class GreenNormalEnemy : public Enemy {
+public:
+    GreenNormalEnemy(int x, int y);
+};
 #endif // REDNORMALENEMY_HPP

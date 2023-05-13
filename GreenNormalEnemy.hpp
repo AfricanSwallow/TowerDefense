d14d9cb10b1cb723021e896a79d8a1b9df483b48
AfricanSwallow <shrubbery@gapp.nthu.edu.tw>
2023-05-13 16:53:30 +0800

    delete GreenNormalEnemy files

--- GreenNormalEnemy.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef GREENNORMALENEMY_HPP
-#define GREENNORMALENEMY_HPP
-#include "Enemy.hpp"
-
-class GreenNormalEnemy : public Enemy {
-public:
-    GreenNormalEnemy(int x, int y);
-};
-#endif // REDNORMALENEMY_HPP

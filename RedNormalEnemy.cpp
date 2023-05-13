6a3f13bf659481a4021092fdfbb1a6c487548c2e
AfricanSwallow <shrubbery@gapp.nthu.edu.tw>
2023-05-13 16:54:03 +0800

    change GreenEnemy speed

--- RedNormalEnemy.cpp
+++ RedNormalEnemy.cpp
@@ -6,6 +6,6 @@ RedNormalEnemy::RedNormalEnemy(int x, int y) : Enemy("play/dice-6.png", x, y, 25
     // TODO 2 (1/3): You can imitate the 2 files: 'RedNormalEnemy.hpp', 'RedNormalEnemy.cpp' to create a new enemy.
 }
 
-GreenNormalEnemy::GreenNormalEnemy(int x, int y) : Enemy("play/dice-5.png", x, y, 25, 45, 7, 5) {
+GreenNormalEnemy::GreenNormalEnemy(int x, int y) : Enemy("play/dice-5.png", x, y, 25, 60, 7, 5) {
     // TODO 2 (1/3): You can imitate the 2 files: 'RedNormalEnemy.hpp', 'RedNormalEnemy.cpp' to create a new enemy.
 }

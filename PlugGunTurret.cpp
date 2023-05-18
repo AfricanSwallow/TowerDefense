604146fd8ea1e0705830c1683aa5f353ff3c9824
AfricanSwallow <shrubbery@gapp.nthu.edu.tw>
2023-05-18 18:47:35 +0800

    Shoot 2 bullets at once

--- PlugGunTurret.cpp
+++ PlugGunTurret.cpp
@@ -60,8 +60,10 @@ void TwoGunTurret::CreateBullet() {
     Engine::Point diff = Engine::Point(cos(Rotation - ALLEGRO_PI / 2), sin(Rotation - ALLEGRO_PI / 2));
     float rotation = atan2(diff.y, diff.x);
     Engine::Point normalized = diff.Normalize();
+    Engine::Point perpendicular = Engine::Point(-normalized.y, normalized.x);
     // Change bullet position to the front of the gun barrel.
-    getPlayScene()->BulletGroup->AddNewObject(new FireBullet(Position + normalized * 36, diff, rotation, this));
+    getPlayScene()->BulletGroup->AddNewObject(new FireBullet(Position + normalized * 36 + perpendicular * 5, diff, rotation, this));
+    getPlayScene()->BulletGroup->AddNewObject(new FireBullet(Position + normalized * 36 - perpendicular * 5, diff, rotation, this));
     // TODO 4 (2/2): Add a ShootEffect here. Remember you need to include the class.
     getPlayScene()->EffectGroup->AddNewObject(new ShootEffect(Position + normalized * 36, diff, rotation, this));
     AudioHelper::PlayAudio("gun.wav");
